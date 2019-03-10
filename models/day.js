aaf8fdc631ab77f63174dc3dfccc16a6271887ed
dianalimnos <dianalim1117@gmail.com>
2019-03-10 01:35:02 -0800

    added mindfulness form

--- models/day.js
+++ models/day.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 const DaySchema = new Schema({
   date: { type: Date, default: Date.now},
+  mindfulness: { type: Number, required: true },
   gratitude1: { type: String, required: true },
   gratitude2: { type: String, required: true },
   gratitude3: { type: String, required: true }
