396c8cd308d083471a748ff1b7eb31d49ccc104d
dianalimnos <dianalim1117@gmail.com>
2019-03-10 06:08:36 -0700

    added new mindfulness feeling why question

--- models/day.js
+++ models/day.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const DaySchema = new Schema({
   date: { type: Date, default: Date.now },
   mindfulness: { type: String, required: true },
+  feeling: { type: String, required: true },
   aspiration: { type: String, required: true },
   gratitude1: { type: String, required: true },
   gratitude2: { type: String, required: true },
